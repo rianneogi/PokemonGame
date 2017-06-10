626d47dd050289e50d98f1cd41246c67262f70ac
Rian Neogi <rianneogi@gmail.com>
2017-06-10 16:56:02 +0530

    fixed vector bug

--- PokemonGame/Vector2.cpp
+++ PokemonGame/Vector2.cpp
@@ -1,7 +1,7 @@
 #include "Vector2.h"
 
 
-Vector2::Vector2(float x = 0.0f, float y = 0.0f)
+Vector2::Vector2(float x, float y)
 {
     this->x = x;
     this->y = y;
--- PokemonGame/Vector2.h
+++ PokemonGame/Vector2.h
@@ -6,7 +6,7 @@ class Vector2
 public:
 	double x;
 	double y;
-	Vector2(float x, float y);
+	Vector2(float x = 0, float y = 0);
     void normalize();
 	double magnitude();
 	double dotProduct(Vector2 other);
